acd8781da6105356fb2497df752e37e72e49aeac
maxhendess <nekohigan@gmail.com>
2023-09-27 13:55:32 +0200

    added TextView

--- app/src/main/java/com/example/yeethubprojet/MainActivity.java
+++ app/src/main/java/com/example/yeethubprojet/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.yeethubprojet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView helloWorld;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
